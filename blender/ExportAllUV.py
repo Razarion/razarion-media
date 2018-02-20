5567970e459fa54f97be0117a72a5c9ebbb777c6
Beat Keller <beat.keller@btxtech.com>
2018-02-20 21:07:47 +0100

    Item color added

--- blender/ExportAllUV.py
+++ blender/ExportAllUV.py
@@ -14,7 +14,7 @@ while i < len(selObj):
  bpy.ops.mesh.select_all(action='TOGGLE')
  bpy.ops.mesh.select_all(action='TOGGLE')
  # bpy.ops.uv.smart_project(angle_limit=66.0, island_margin=0.0, user_area_weight=0.0)
- f="C:\\dev\\projects\\razarion\\razarion-media\\gimp\\items\\structures\\UV\\uv_" + bpy.data.objects[selObj[i]].name
+ f="C:\\dev\\projects\\razarion\\razarion-media\\gimp\\exportedUv\\uv_" + bpy.data.objects[selObj[i]].name
  bpy.ops.uv.export_layout(filepath=f, mode='PNG', size=(512, 512), opacity=0)
  bpy.ops.object.mode_set(mode="OBJECT")
  bpy.ops.object.select_all(action='TOGGLE')
